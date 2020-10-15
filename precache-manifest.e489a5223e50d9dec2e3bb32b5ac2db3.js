02947514c3967c3770c8b0681d6dc348f1db9865
U-Masaki-ta <etamaniuta@gmail.com>
2020-10-15 16:58:36 +0900

    Updates

---
+++ precache-manifest.e489a5223e50d9dec2e3bb32b5ac2db3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e419cab2000afc27466a8de190af53",
+    "url": "/simple-todo/index.html"
+  },
+  {
+    "revision": "8bace70e6bffd5132a34",
+    "url": "/simple-todo/static/css/main.776d571a.chunk.css"
+  },
+  {
+    "revision": "48743d0ba2832778c5fa",
+    "url": "/simple-todo/static/js/2.fd19c48f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simple-todo/static/js/2.fd19c48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bace70e6bffd5132a34",
+    "url": "/simple-todo/static/js/main.f60d5883.chunk.js"
+  },
+  {
+    "revision": "a9b828f3067ebb10d15b",
+    "url": "/simple-todo/static/js/runtime-main.b107b839.js"
+  }
+]);
